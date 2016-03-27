bc73b59f6912c832f228dd8c7022c2b79b3a1833
mantovanleonardo <mantovanleonardo@gmail.com>
2016-03-27 16:50:30 -0300

    Agrego los tests de la entrega 0

---
+++ src/test/java/ar/edu/utn/d2s/2016_dds_utn/src/Puntos_de_Interes.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Puntos_de_Interes {
+
+	String nombre = "Leonardo";
+	Object punto;
+
+	@Test
+	public void nombreVacio() {
+		Puntos_de_Interes puntos_interes = new Puntos_de_Interes();
+
+		if (puntos_interes.nombre == null) {
+			fail("El nombre esta en null");
+		} else {
+			if (puntos_interes.nombre.isEmpty() == true) {
+				fail("El nombre es vacío");
+			}
+		}
+	}
+
+	@Test
+	public void masDe60caracteres() {
+		Puntos_de_Interes puntos_interes = new Puntos_de_Interes();
+
+		if (puntos_interes.nombre == null) {
+			fail("El nombre esta en null");
+		} else {
+			if (puntos_interes.nombre.length() > 60) {
+				fail("El nombre es tiene mas de 60 caracteres");
+			}
+		}
+	}
+
+	@Test
+	public void puntoNull() {
+		Puntos_de_Interes puntos_interes = new Puntos_de_Interes();
+
+		if (puntos_interes.punto == null) {
+			fail("El punto de interes no puede ser null");
+		}
+	}
+
+}
